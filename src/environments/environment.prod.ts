f8c9779d5b1e9f410083faa5a8bd8b90850a74ac
aoaclothings7 <oluwatobiadejugbe7@gmail.com>
2023-07-28 14:57:43 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  productCategory: 'http://localhost:3000/product-category',
+  user: 'http://localhost:3000/user',
+  product_category: 'http://localhost:3000/productCategory',
+  vendors: 'http://localhost:3000/vendors',
   warehouse: 'http://localhost:3000/warehouse',
 };
