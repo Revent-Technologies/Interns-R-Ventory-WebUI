e8c0978c730f832ebcbd0c6a2ec4bceb221accad
Kolawole <omotoshok9@gmail.com>
2023-07-30 03:50:37 +0100

    Dashboard in progress

--- src/app/@components/webapp/webapp.module.ts
+++ src/app/@components/webapp/webapp.module.ts
@@ -5,17 +5,17 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { WebappComponent } from './webapp.component';
 import { HeaderModule } from './header/header.module';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromWarehouseEffects from 'src/app/@core/stores/warehouse/warehouse.effects';
-
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
-  declarations: [WebappComponent, SidebarComponent, DashboardComponent],
+  declarations: [WebappComponent, SidebarComponent],
   imports: [
     CommonModule,
     WebappRoutingModule,
     MatExpansionModule,
+    DashboardModule,
     HeaderModule,
     EffectsModule.forFeature([fromWarehouseEffects.WarehouseEffects]),
   ],
