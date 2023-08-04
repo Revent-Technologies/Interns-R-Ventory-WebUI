55ea75b02b5d87ed8e6472296429c65356d2b5e5
Kolawole <omotoshok9@gmail.com>
2023-08-04 13:06:19 +0100

    More Refactoring

--- src/app/@core/services/warehouse.service.ts
+++ src/app/@core/services/warehouse.service.ts
@@ -1,8 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
-import { Warehouse } from '../interfaces';
-import { Store } from '@ngrx/store';
+
 
 @Injectable({
   providedIn: 'root',
